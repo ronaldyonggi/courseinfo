6f6711da4358f18de88b0adf8a41fcfd319f9be7
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-10 11:19:06 -0400

    2.2 completed

--- src/appComponents/courseComponents/Total.js
+++ src/appComponents/courseComponents/Total.js
@@ -1,7 +1,7 @@
 const Total = ({parts}) => {
   const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return (
-    <p>Number of exercises {total}</p>
+    <p><span style={{fontWeight: 'bold'}}>total of {total} exercises</span></p>
   )
 }
 
