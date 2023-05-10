4111f349ffe220a7a8a64bb06a83b5c5ee65564e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-10 11:40:24 -0400

    2.4 completed

--- src/appComponents/courseComponents/Content.js
+++ src/appComponents/courseComponents/Content.js
@@ -3,9 +3,9 @@ import Part from "./contentComponents/Part"
 const Content = ({parts}) => {
   return (
     <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises}/>
-      <Part name={parts[1].name} exercises={parts[1].exercises}/>
-      <Part name={parts[2].name} exercises={parts[2].exercises}/>
+      {parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
